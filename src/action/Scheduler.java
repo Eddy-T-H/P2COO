846e207b3dc78cf36bc5147a1750c264ecdb2d6a
Eddy T <e.thockler@gmail.com>
2015-10-20 16:23:20 +0200

    MLG

--- src/action/Scheduler.java
+++ src/action/Scheduler.java
@@ -1,67 +1,25 @@
 package action;
 
-import java.util.ArrayList;
-import java.util.List;
 
-public abstract class Scheduler extends Action {
-	
-	//ATTRIBUTS
-	protected List<Action> actions;
-	
-	//CONSTRUCTORS
-	/**
-	 * Create a new Scheduler with an empty list of Action
-	 */
-	public Scheduler(){
-		super();
-		this.actions = new ArrayList<Action>();
-	}
-	
-	/**
-	 * Create a new Scheduler with a list of Actions given as a parameter
-	 * @param actions, the List of action you want to schedule
-	 */
-	public Scheduler(List<Action> actions){
-		super();
-		this.actions = actions;
-	}
-	
-	//METHODS
-	
-	/**
-	 * add an action to this Scheduler
-	 * @param a , the action you want to add
-	 */
-	public abstract void addAction(Action a );
-	
-	/**
-	 * the assertion that every action of this scheduler is ready
-	 * @return true if every action of this scheduler is ready
-	 * @return false if no
-	 */
+public class Scheduler extends Ordonnanceur {
+
 	@Override
-	public boolean isReady() {
-		boolean resultat = true;
-		for(Action action : this.actions){
-			resultat = resultat && action.isReady();
+	public void internalStep() {
+		Action action;
+		action = this.getActions().get(this.incrementIndex());
+		action.step();
+		if(action.isOver()){
+			this.getActions().remove(action);
+			this.decrementIndex();
 		}
-		return resultat;
+		System.out.print(""+this.getMessage());
 	}
-
+	
 	/**
-	 * the assertion that every action of this scheduler is finished
-	 * @return true if every action of this scheduler is finished 
-	 * @return false if no
 	 */
-	@Override
-	public boolean isFinished() {
-		boolean resultat = true;
-		for(Action action : this.actions){
-			resultat = resultat && action.isFinished();
-		}
-		return resultat;
+	public void add(Action action){
+		this.getActions().add(this.getCurrentActionIndex(), action);
+		this.incrementIndex();
 	}
-
-	@Override
-	protected abstract void reallyDoStep() throws ActionFinishedException;
-	}
+	
+}
